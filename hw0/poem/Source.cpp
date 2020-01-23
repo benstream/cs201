5ce07bc1187262e91ef100cbeaf6a70c22b942fc
Benjamin Stream <brstream@alaska.edu>
2020-01-22 21:54:47 -0900

    First they came, It works!

---
+++ hw0/poem/Source.cpp
@@ -0,0 +1,31 @@
+/*
+Benjamin Stream
+Jon Genneti
+CS201
+January 22, 2020
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+int main()
+{
+	cout << "First They Came - Pastor Martin Niemoller" << endl;
+	cout << " " << endl;
+	cout << "First they came for the Communists" << endl;
+	cout <<	"And I did not speak out" << endl;
+	cout << "Because I was not a Communist" << endl;
+	cout << "Then they came for the Socialists" << endl;
+	cout << "And I did not speak out" << endl;
+	cout << "Because I was not a Socialist" << endl;
+	cout << "Then they came for the trade unionists" << endl;
+	cout << "And I did not speak out" << endl;
+	cout << "Because I was not a trade unionist" << endl;
+	cout << "Then they came for the Jews" << endl;
+	cout << "And I did not speak out" << endl;
+	cout << "Because I was not a Jew" << endl;
+	cout << "Then they came for me" << endl;
+	cout << "And there was no one left" << endl;
+	cout << "To speak out for me" << endl;
+	return 0;
+}
