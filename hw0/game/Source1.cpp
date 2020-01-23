125344794e264aff6b059e177efe430cd5884d04
Benjamin Stream <brstream@alaska.edu>
2020-01-22 21:42:04 -0900

    Alright I figured it out!

---
+++ hw0/game/Source1.cpp
@@ -0,0 +1,14 @@
+/*
+Benjamin Stream
+Jon Genetti
+CS201
+January 22, 2019
+*/
+#include <iostream>
+using std::cout;
+using std::endl;
+int main()
+{
+	cout << "I love minecraft. It is a truely creative game, with many possibilities of how you want to play. If you want to be a simple farmer you can, or you can be a so called god of everything. The world is yours to explore and mine." << endl;
+	return 0;
+}
