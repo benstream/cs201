0a49c22d20b90a6f87ae911796ef4223646aa24e
Benjamin Stream <brstream@alaska.edu>
2020-01-22 22:23:57 -0900

    Wrote the program, time to see if it works.

---
+++ hw0/hello world/Source.cpp
@@ -0,0 +1,20 @@
+/*
+Benjamin Stream
+Jon Genneti
+CS201
+January 22, 2020
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	cout << "Hello world!" << endl;
+	return 0;
+}
