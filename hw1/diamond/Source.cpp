adc0f76b348b60e79c91db1702b96b6ac35febc0
Benjamin Stream <brstream@alaska.edu>
2020-02-02 16:36:17 -0900

    testing out making sure I set up the cpp file correctly and compiles.

---
+++ hw1/diamond/Source.cpp
@@ -0,0 +1,17 @@
+/*
+Benjamin Stream
+Jon Genetti
+CS201
+diamond.cpp
+February 2, 2020
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+int main()
+{
+	cout << "Hi" << endl;
+		return 0;
+}
