35c097d8349b51dbbc31323bcb391c066e220a09
Benjamin Stream <brstream@alaska.edu>
2020-02-02 18:12:19 -0900

    Attempting to get error handling figured out

--- hw1/diamond/Source.cpp
+++ hw1/diamond/Source.cpp
@@ -10,8 +10,32 @@ February 2, 2020
 using std::cout;
 using std::endl;
 using std::cin;
+
+//Declaration of variables
+
+int lines = 0; 
+
 int main()
 {
-	cout << "Hi" << endl;
-		return 0;
+	cout << "Please type a whole positive number above zero:" << endl;
+
+	// Function to notify user of an error with input.	
+		
+		while (lines <= 0)
+		{
+			if (!(cin >> lines) or lines <= 0)
+				{
+
+					cin.clear();
+					cin.ignore();
+					cout << "What you entered is not a whole positive number above zero" << endl;
+					cout << "Please type a whole positive number above zero:" << endl;
+					
+		}
+	}
+	// Function for building the diamond.
+
+	cout << lines << endl;
+	
+
 }
