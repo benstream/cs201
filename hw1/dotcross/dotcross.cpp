56fd048db485d5dd0bb1264028fcfeec808116a4
Benjamin Stream <brstream@alaska.edu>
2020-02-02 23:46:51 -0900

    Error Handling Works

--- hw1/dotcross/dotcross.cpp
+++ hw1/dotcross/dotcross.cpp
@@ -9,18 +9,84 @@ February 2, 2020
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <math.h>
+#include <cfloat>
+#include <cstdlib>
 
 using std::cout;
 using std::endl;
 using std::cin;
 
 // Variables
-float xa, ya, za, xb, yb, zb = 0;
-std::string error;
-float cx, cy, cz = 0; // Reversed naming scheme to making easier to identify the product from the inputs.
+float storage = NULL; // Stores values before passing them on
+float xa, ya, za, xb, yb, zb = NULL;
+std::string error = "0";
+float cx, cy, cz = NULL; // Reversed naming scheme to making easier to identify the product from the inputs.
+
 
 int main()
 {
-	
+    cout << "Please enter values for your dots." << endl;
+    while (cx == NULL or cy == NULL or cz == NULL)
+            {
+        // Error Handling
+            try 
+                {
+                    storage = std::stof(error);
+                    if (xa == NULL) 
+                    {
+                        xa = storage;
+                    }
+                    else if (ya == NULL)
+                    {
+                        ya = storage;
+                    }
+                    else if (zb == NULL)
+                    {
+                        zb = storage;
+                    }
+                    if (xb == NULL)
+                    {
+                        xb = storage;
+                    }
+                    else if (yb == NULL)
+                    {
+                        yb = storage;
+                    }
+                    else if (zb == NULL)
+                    {
+                        zb = storage;
+                    }
+                }
+            catch (...)
+            {
+                cout << "Invalid value, try again" << endl;
+            }
+            if (xa == NULL)
+            {
+                cout << "Enter value for Ax: ";
+                cin >> error;
+        
 
+            }
+            else if (ya == NULL)
+                {
+                    cout << "Enter Value for Ay: ";
+                        cin >> error;
+                }
+            else if (za == NULL)
+            {
+                cout << "Enter Value for Az: ";
+                cin >> error;
+            }
+            else if (xb == NULL)
+            {
+                cout << "Enter Value for Bx: ";
+                cin >> error;
+            }
+        
+                }
+
+        
+		
 }
