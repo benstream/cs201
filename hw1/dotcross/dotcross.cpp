7547ff163da66dcf613fbfa02d222fb98fd0f6f5
Benjamin Stream <brstream@alaska.edu>
2020-02-02 22:17:48 -0900

    setting up error processing

---
+++ hw1/dotcross/dotcross.cpp
@@ -0,0 +1,26 @@
+/*
+Benjamin Stream
+Jon Genetti
+CS201
+dotcross.cpp
+February 2, 2020
+*/
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+// Variables
+float xa, ya, za, xb, yb, zb = 0;
+std::string error;
+float cx, cy, cz = 0; // Reversed naming scheme to making easier to identify the product from the inputs.
+
+int main()
+{
+	
+
+}
