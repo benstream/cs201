380138cd6b274839e1c202077cfc9e33068499d5
Benjamin Stream <brstream@alaska.edu>
2020-02-03 18:41:53 -0900

    Added look to redo when error is found.

--- hw1/mileskm/mileskm.cpp
+++ hw1/mileskm/mileskm.cpp
@@ -17,19 +17,22 @@ float miles, kilometers = 0;
 int done = 0;
 int main()
 {
-	cout << "Enter number of miles for conversion: ";
+	while (done == 0)
+	{
+		cout << "Enter number of miles for conversion: ";
 		cin >> miles;
 		kilometers = (miles / 1.609); //Calculates the conversion
-			cout << "This is " << kilometers << " kilometers." << endl;
-			if (!cin) //checks for valid input
-			{
-				cout << "That was no valid input! Please Try again. " << endl;;
-				cin.clear();
-				cin.ignore();
+		cout << "This is " << kilometers << " kilometers." << endl;
+		if (!cin) //checks for valid input
+		{
+			cout << "That was no valid input! Please Try again. " << endl;;
+			cin.clear();
+			cin.ignore();
 
-			}
-			else
-			{
-				done = 0
-			}
+		}
+		else
+		{
+			done = 1;
+		}
+	}
 }
