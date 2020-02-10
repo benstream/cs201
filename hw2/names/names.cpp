82162c1996f53eea4d3f096e5e9c3a7b0ed8aaa0
Benjamin Stream <brstream@alaska.edu>
2020-02-09 15:23:09 -0900

    Wrote the example program that inputs 10 names

---
+++ hw2/names/names.cpp
@@ -0,0 +1,32 @@
+/*
+Benjamin Stream
+Jon Genetti
+CS201
+names.cpp
+2 9, 2020
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include<algorithm>
+
+using std::cout;
+using std::endl;
+using std::cin;
+using std::vector;
+using std::string;
+
+int main(int argc, char **argv)
+{
+	vector<string> names;
+	for (int i = 0; i < 10; i++)
+	{
+		string name;
+		cout << "Please enter a name: ";
+		std::getline(cin, name);
+		names.push_back(name);
+
+	}
+	return 0;
+}
