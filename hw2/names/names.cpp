6021ef03f67a6706303f307e08d52920466b284c
Benjamin Stream <brstream@alaska.edu>
2020-02-09 15:46:18 -0900

    Testing DoesNameExist()

--- hw2/names/names.cpp
+++ hw2/names/names.cpp
@@ -17,17 +17,20 @@ using std::cin;
 using std::vector;
 using std::string;
 
-void InputNames(vector<string>& names);
-
+void InputNames(vector<string> & names);
+bool DoesNameExist(const string & nameToFind, const vector<string> & names);
 
 int main(int argc, char **argv)
 {
 	vector<string> names;
+	string nameToFind;
 	InputNames(names);
+	cout << endl << "Please Type a name to find: ";
+	cout << "This name is in the list: " << DoesNameExist(nameToFind, names) << endl;
 	return 0;
 }
 
-void InputNames(vector<string>& names)
+void InputNames(vector<string> & names) // Function to input names
 {
 
 	for (int i = 0; i < 10; i++)
@@ -38,3 +41,17 @@ void InputNames(vector<string>& names)
 		names.push_back(name);
 	}
 }
+
+bool DoesNameExist(const string & nameToFind, const vector<string> & names) // Function set up to find names within vector 
+	{
+
+	for (string name : names)
+	{
+		if (name == nameToFind)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	}
