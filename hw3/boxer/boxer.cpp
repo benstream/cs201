482b72186bc46656d465a32cc97b6bd3679d7160
Benjamin Stream <brstream@alaska.edu>
2020-02-17 17:10:43 -0900

    Attempted printing box function

--- hw3/boxer/boxer.cpp
+++ hw3/boxer/boxer.cpp
@@ -8,6 +8,7 @@ February 17, 2020
 
 #include <iostream>
 #include <string>
+#include "boxer.h"
 using std::cout;
 using std::endl;
 using std::cin;
@@ -15,16 +16,31 @@ using std::string;
 
 void topBottom(std::string message, int layers) // For the top and bottom layers of the box.
 {
-	string astrisk(layers, '*');
+	string asterisk(layers, '*');
 		for (int i = 0; i < layers; i++)
 		{
 			cout << "*";
 		}
-	cout << astrisk << endl;
+	cout << asterisk << endl;
 
 }
 
 void printBox(std::string message, int layers) // to print the box body.
 {
-
-}
+	string asterick(layers, '*'); // for the left and right of text
+	topBottom(message, layers);
+	cout << asterick;
+	for (int j = 0; j < message.length() + 2; j++)
+	{
+		cout << " ";
+	}
+	
+	cout << asterick << endl
+		<< asterick << " " << message << " " << asterick
+		<< endl << asterick;
+	for (int j = 0; j < message.length() + 2; j++)
+	{
+		cout << " ";
+	}
+	cout << asterick << endl;
+}	
