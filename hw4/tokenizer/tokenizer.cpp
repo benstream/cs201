982afe9b33be78892c93d29b29f23336b999c359
Benjamin Stream <brstream@alaska.edu>
2020-02-26 18:01:40 -0900

    wrote AnalyseTokens();

--- hw4/tokenizer/tokenizer.cpp
+++ hw4/tokenizer/tokenizer.cpp
@@ -28,9 +28,32 @@ unsigned StringToTokenWS(vector<string>& tokens)
 		while (instream)
 		{
 			instream >> token;
-			if (token == "end" || token == "End")
+			if (token == "end" || token == "End") return tokens.size();
 				tokens.push_back(token + " ");
 		}
 	}
 	return 99;
+}
+
+void AnalyseTokens(const vector<string>& tokens)
+{
+	vector<string>categories = { "[integer]","[identifier]","[string]","[whitespace]","[unknown]" };
+	int first = 6;
+	int current = 6;
+	int i = 0;
+		for (string token : tokens)
+		{
+			if (token[0] <= '9' && token[0] >= '0')
+			{
+				for (char c : token)
+				{
+					if (c >= '9' || c <= '0')
+					{
+						cout << categories[5];
+					}
+				}
+			}
+			i++;
+		}
+
 }
