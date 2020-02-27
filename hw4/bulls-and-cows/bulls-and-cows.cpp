9aad1e9f783fbe5f30ba4126128a9d8fb12c6a6f
Benjamin Stream <brstream@alaska.edu>
2020-02-26 21:56:38 -0900

    Created File and added variables

---
+++ hw4/bulls-and-cows/bulls-and-cows.cpp
@@ -0,0 +1,28 @@
+/*
+Benjamin Stream
+Jon Genetti
+CS201
+bulls-and-cows.cpp
+February 26, 2020
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+using std::cout;
+using std::endl;
+using std::cin;
+using std::vector;
+
+int main()
+{
+	vector<int> secrets = { 1,2,3,4 };
+	vector<int> guesses;
+	int guess;
+	int bulls = 0;
+	int cows = 0;
+	while (true)
+
+
+
+}
