8edff145efbdacce5c71be023c9af83a1a873085
Benjamin Stream <brstream@alaska.edu>
2020-02-26 22:23:04 -0900

    Finished the progam, fixed up formatting, added negative checker and looping

--- hw4/bulls-and-cows/bulls-and-cows.cpp
+++ hw4/bulls-and-cows/bulls-and-cows.cpp
@@ -18,27 +18,46 @@ int main()
 {
 	vector<int> secrets = { 1,2,3,4 };
 	vector<int> guesses;
-	int guess;
+	int guess{};
 	int bulls = 0;
 	int cows = 0;
+	while (bulls != 4)
+	{
+	
 	while (true)
 	{
-		for (int i = 0; i < 4; i++) // loop to ask the values
-		{
-			cout << "Guess the value at " << i << ":";
-			cin >> guess;
-			guesses.push_back(guess);
+			if (guess < 0) for (int i = 0; i < 4; i++)
+			{
+				cout << secrets[i];
+				return 0;
+			}
+			for (int i = 0; i < 4; i++) // loop to ask the values
+			{
+				cout << "Guess the value at " << i + 1 << ": ";
+				cin >> guess;
+				if (guess < 0)
+				{
+					cout << "The correct numbers are:" << secrets[0] << secrets[1] << secrets[2] << secrets[3];
+					return 0;
+				}
+				guesses.push_back(guess);
+			}
+
+			for (int i = 0; i < 4; i++)
+			{
+				if (secrets[i] == guesses[i]) bulls++;
+				else cows++;
+			}
+			cout << bulls << " bulls and " << cows << " cows" << endl;
+			if (bulls == 4)
+			{
+				return 0;
+
+			}
+			cows = 0;
+			bulls = 0;
+			guesses.clear();
 		}
-		for (int i = 0; i < 4; i++)
-		{
-			if (secrets[i] == guesses[i]) bulls++;
-			else if (guesses[i] == secrets[1] || guesses[i] == secrets[2] || guesses[i] == secrets[3]) cows++;
-		}
-		cout << bulls << " bulls and" << cows << " cows" << endl;
-		if (bulls == 4) return 0;
-		cows = 0;
-		bulls = 0;
-		guesses.clear();
 	}
 
 
