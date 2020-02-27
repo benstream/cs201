030407591f82e96abde717a37bf7a15bbab90563
Benjamin Stream <brstream@alaska.edu>
2020-02-26 22:51:56 -0900

    Added FifoPop();

--- hw4/fifo-lifo/fifo-lifo.cpp
+++ hw4/fifo-lifo/fifo-lifo.cpp
@@ -47,5 +47,30 @@ void FifoPush(vector<string>& container, const string& item)
 
 void FifoPop(vector<string>& container, string& item)
 {
+	if (IsContainerEmpty(container) == true)
+	{
+		vector<string> TempStorage(container.size() - 1); // Temporary Storage to hold items.
+		for (int i = 1; i < container.size(); i++)
+		{
+			TempStorage[i - 1] = container[i];
+			cout << endl << "Last item in the list removed." << endl;
+			cout << endl << "Items in FIFO: " << endl;
+		}
+	}
+	else
+	{
+		cout << endl << "There is nothing in this container! Please put something in it." << endl << endl;
+	}
+}
+
+// Functions, Last in First Out.
+
+void LifoPush(vector<string>& container, const string& item)
+{
+
+}
+
+void LifoPop(vector<string>& container, string& item)
+{
 
 }
