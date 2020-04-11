e30e7eebd6286fe975805e202800ff4d63ab3bd3
Benjamin Stream <brstream@alaska.edu>
2020-04-10 19:46:12 -0800

    set up layout

---
+++ hw6/hangman/hangman/hangman.cpp
@@ -0,0 +1,21 @@
+/*
+Benjamin Stream
+Jon Genetti
+CS201
+hangman.cpp
+April 10, 2020
+*/
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
+#include <vector>
+#include <map>
+using namespace std;
+
+int main()
+{
+
+}
