74837f14457fc44e878c3f50e2d781edba9d5168
Benjamin Stream <brstream@alaska.edu>
2020-04-10 19:01:25 -0800

    Created printInventory and buy function

--- hw6/shopping/shopping.cpp
+++ hw6/shopping/shopping.cpp
@@ -28,3 +28,26 @@ int main()
 {
 
 }
+
+void printInventory(std::map<std::string, Record>& inventory)
+{
+	for (auto item : inventory)
+	{
+		cout << "We have " << item.second.units << " " << item.first << "s for the low price of " << item.second.unitPrice << endl;
+	}
+}
+
+void buy(std::map<std::string, Record>& inventory, double& total)
+{
+	cout << "What would you like to buy? ";
+	std::string item;
+	if (inventory[item].units > 0)
+	{
+		inventory[item].units -= 1;
+		total += inventory[item].unitPrice;
+	}
+	else
+	{
+		cout << "We don't have any more of your requested item";
+	}
+}
