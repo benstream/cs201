7d616ecfbbdbe9e6ebabcd949a34a42a417a554b
Benjamin Stream <brstream@alaska.edu>
2020-04-14 15:38:01 -0800

    Game Engine Dubbed "Bengine"

--- Final_Project/2D_GameEngine/2D_GameEngine/game.cpp
+++ Final_Project/2D_GameEngine/2D_GameEngine/game.cpp
@@ -25,6 +25,7 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 		renderer = SDL_CreateRenderer(window, -1, 0);
 		if (renderer)
 		{
+			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 			std::cout << "Renderer Created" << std::endl;
 		}
 		isRunning = true;
--- Final_Project/2D_GameEngine/2D_GameEngine/main.cpp
+++ Final_Project/2D_GameEngine/2D_GameEngine/main.cpp
@@ -5,9 +5,21 @@ CS201 Jon Genetii
 */
 #include "SDL.h"
 #include "iostream"
+#include "game.h"
+
+Game* game = nullptr;
 
 int main(int argc, char* argv[])
 {
+	game = new Game();
+	game->init("Bengine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+	while (game->running()) // Loop for game running
+	{
+		game->handleEvents();
+		game->update();
+		game->render();
+	}
+	game->clean();
 	
 	return 0;
 
