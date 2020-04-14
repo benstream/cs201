d153216f9d34af93125e366866fa769030740d4b
Benjamin Stream <brstream@alaska.edu>
2020-04-14 14:57:23 -0800

    Added to classes in game.cpp

--- Final_Project/2D_GameEngine/2D_GameEngine/game.cpp
+++ Final_Project/2D_GameEngine/2D_GameEngine/game.cpp
@@ -32,19 +32,38 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 	
 	else 
 	{
-		isRunning = false;
+		isRunning = false; 
 	}
 }
 
 void Game::handleEvents()
-{}
+{
+	SDL_Event event;
+	SDL_PollEvent(&event);
+	switch (event.type)
+	{
+	case SDL_QUIT:
+		isRunning = false;
+		break;
+
+	}
+}
 
 void Game::update()
 {}
 
 void Game::render()
-{}
+{
+	SDL_RenderClear(renderer);
+	// Add stuff to render here
+	SDL_RenderPresent(renderer);
+}
 
 void Game::clean()
-{}
+{
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+	std::cout << "Game Cleaned." << std::endl;
+}
 
