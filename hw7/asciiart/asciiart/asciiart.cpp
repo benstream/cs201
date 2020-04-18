5d7af559d84e3d978e4f408550b1163e9acee4d2
Benjamin Stream <brstream@alaska.edu>
2020-04-17 18:16:35 -0800

    Added All the Files

---
+++ hw7/asciiart/asciiart/asciiart.cpp
@@ -0,0 +1,16 @@
+/*
+Benjamin Stream
+Jon Genetti
+CS201
+program.cpp
+Month Day, 2020
+*/
+
+#include <iostream>
+#include "Color3.hpp"
+#include "Image3.hpp"
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
