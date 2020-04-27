c099d913ee74d515f0af1ba68d8f048623da9c44
Benjamin Stream <brstream@alaska.edu>
2020-04-26 23:38:21 -0800

    Class Done for ECS

--- Final_Project/2D_GameEngine/2D_GameEngine/ECS.h
+++ Final_Project/2D_GameEngine/2D_GameEngine/ECS.h
@@ -23,10 +23,10 @@ inline ComponentID getComponentID()
 	return lastID++;
 }
 
-template <typename T> inline ComponentID getComponentID() noexcept 
+template <typename T> inline ComponentID getComponentTypeID() noexcept 
 {
 
-	satic ComponentID typeID = getComponentID();
+	satic ComponentID typeID = getComponentTypeID();
 	return typeID();
 }
 
@@ -55,5 +55,71 @@ private:
 
 	ComponentArray componentArray;
 	ComponentBitSet componentBitSet;
+public:
+	void update()
+	{
+		for (auto& c : components) c->update();
+		for (auto& c : components) c->draw();
+	}
+	void draw() {}
+	bool isActive() { return active; }
+	void destroy() { active = false; } // Allows us to destroy inactive entities so we can save system resources
+	template <typename T> bool hasComponent() const
+	{
+		return componentBitSet[getComponentID<T>];
+	}
 
+	template <typename T, typename... TArgs>
+	T& addComponent(TArgs&&...mArgs)
+	{
+		T* c(new T(std::forward<TArgs>(mArgs)...));
+		c->entity = this;
+		std::unique_ptr<Component> uPtr{ c };
+		components.emplace_back(std::move(uPtr));
+
+		componentArray[getComponentTypeID<T>()] = c;
+		componentBitSet[getComponentTypeID<T>()] = true;
+
+		c->init();
+		return *c;
+	}
+
+	template<typename T> T& getComponent() const
+	{
+		auto ptr(componentArray[getComponentTypeID<t>()])
+			return *static_cast<T*>(ptr);
+	}
+};
+
+class Manager // Using manager to update indivitual entities
+{
+private:
+	std::vector<std::unique_ptr<Entity>> entities;
+
+public:
+	void update() //updates entities
+	{
+		for (auto& e : entities) e->update();
+	}
+	void draw() //draws the entities
+	{
+		for (auto& e : entities)e->draw();
+	}
+	
+	void refresh() // removes non-active entities
+	{
+		entities.erase(std::remove_if(std::begin(entities), std::end(entities),
+			[](const std::unique_ptr<Entity>& mEntity)
+			{
+				return !mEntity->isActive();
+			}),
+			std::end(entities));
+	}
+	Entity& addEntity()
+	{
+		Entity* e = new Entity();
+		std::unique_ptr<Entity> uPtr{ e };
+		entities.emplace_back(std::move(uPtr));
+		return *e;
+	}
 };
