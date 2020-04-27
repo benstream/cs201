439b84447828ce613e7653b60ca90c96c854312a
Benjamin Stream <brstream@alaska.edu>
2020-04-26 19:49:08 -0800

    Created GameObject.cpp

--- Final_Project/2D_GameEngine/2D_GameEngine/game.cpp
+++ Final_Project/2D_GameEngine/2D_GameEngine/game.cpp
@@ -1,8 +1,9 @@
 #include "game.h"
 #include "TextureManager.h"
+#include "GameObject.h"
 
-SDL_Texture* playerTex;
-SDL_Rect srcR, destR;
+GameObject* player;
+
 
 Game::Game()
 {}
@@ -34,7 +35,7 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 		isRunning = true;
 	} 
 
-	playerTex = TextureManager::LoadTexture("assets/test.png", renderer);
+	player = new GameObject("assets/player.png", renderer, 0,0);
 	
 	/*else 
 	{
@@ -57,18 +58,13 @@ void Game::handleEvents()
 
 void Game::update()
 {
-	cnt++;
-	destR.h = 320;
-	destR.w = 320;
-	destR.x = cnt;
-
-	std::cout << cnt << std::endl;
+	player->Update();
 }
 
 void Game::render()
 {
 	SDL_RenderClear(renderer);
-	SDL_RenderCopy(renderer, playerTex, NULL, &destR);
+	player->Render();
 	SDL_RenderPresent(renderer);
 }
 
