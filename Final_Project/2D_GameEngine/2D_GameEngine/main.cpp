f17ff62712bbbadabe69c44f391161ef32748c58
Benjamin Stream <brstream@alaska.edu>
2020-04-26 19:00:33 -0800

    Added frame limiter to 60FPS

--- Final_Project/2D_GameEngine/2D_GameEngine/main.cpp
+++ Final_Project/2D_GameEngine/2D_GameEngine/main.cpp
@@ -14,13 +14,26 @@ Game* game = nullptr;
 
 int main(int argc, char* argv[])
 {
+	const int FPS = 60; // 60fps Master Race submit sub 60 FPS systems.
+	const int frameDelay = 1000 / FPS;
+	Uint32 frameStart; // Massive Integer 
+	int frameTime;
+
 	game = new Game();
 	game->init("Bengine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080, false);
 	while (game->running()) // Loop for game running
 	{
+		frameStart = SDL_GetTicks();
 		game->handleEvents();
 		game->update();
 		game->render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameDelay > frameTime) // Checks if the game is running too fast and corrects the difference.
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 	game->clean();
 	
